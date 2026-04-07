286c6d18d22162ea559d35cf6d29f3e7866674eb
George <george.d.koutroumpis@gmail.com>
2026-04-07 18:29:40 +0300

    A second commit

---
+++ Lets_try_for_Git/src/main/java/com/mycompany/lets_try_for_git/Lets_try_for_Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lets_try_for_git;
+
+/**
+ *
+ * @author admin
+ */
+public class Lets_try_for_Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
